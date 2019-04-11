4cd3956a6ee19362a26c5b8c3e3fe9eb28c26f8d
zhouzp2115 <1169605579@qq.com>
2019-04-11 14:16:06 +0800

    grad

--- cifar-10/CIFAR10_Net.py
+++ cifar-10/CIFAR10_Net.py
@@ -188,9 +188,10 @@ class CIFAR10_Net():
             for parameters in self.netD.parameters():
                 D_grad_item.append(parameters.grad.clone().detach())
             D_grad.append(D_grad_item)
+            self.optimizerD.step()
             
-        self.setgrad(D_grad,self.netD)
-        self.optimizerD.step()
+        #self.setgrad(D_grad,self.netD)
+        #self.optimizerD.step()
 
         for index, data in enumerate(batch_data):
             G_grad_item = []
@@ -213,9 +214,10 @@ class CIFAR10_Net():
             if (index+1) % 50 == 0:
                 print('Loss_D: %.4f\tLoss_G: %.4f'
                       % (sum(D_losses_batch).item() / (index+1), sum(G_losses_batch).item() / (index+1)))
+            self.optimizerG.step()
         
-        self.setgrad(G_grad,self.netG)
-        self.optimizerG.step()
+        #self.setgrad(G_grad,self.netG)
+        #self.optimizerG.step()
 
         self.G_losses.append(sum(G_losses_batch).item() / len(G_losses_batch))
         self.D_losses.append(sum(D_losses_batch).item() / len(D_losses_batch))
