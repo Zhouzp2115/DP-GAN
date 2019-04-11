12d4c4a08cdc02afbc20a1d046d505543e793933
zhouzp2115 <1169605579@qq.com>
2019-04-11 11:16:47 +0800

    GAN

--- GAN.py
+++ GAN.py
@@ -131,7 +131,7 @@ def train():
     #plt.show()
     
     #print data from dataloader
-    print(real_batch[0].to(device)[:64])
+    print('data from dataloader before train\n',real_batch[0].to(device)[:64])
 
     netG = Generator(ngpu).to(device)
     netD = Discriminator(ngpu).to(device)
@@ -188,7 +188,7 @@ def train():
            label = torch.full((b_size,), real_label, device=device)
            
            #print data from  in train
-           print(real_cpu)
+           print('train data\n',real_cpu)
            
            # Forward pass real batch through D
            output = netD(real_cpu).view(-1)
@@ -205,8 +205,7 @@ def train():
            fake = netG(noise)
 
            #print fake  in train
-           print(fake)
-           exit()
+           print('fake img\n',fake)
 
            label.fill_(fake_label)
            # Classify all fake batch with D
@@ -241,6 +240,8 @@ def train():
                print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
                   % (epoch, num_epochs, i, batch_size,
                      sum(D_losses_batch).item()/i, sum(G_losses_batch).item()/i))
+            
+           break
 
         # Save Losses for plotting later
         G_losses.append(sum(G_losses_batch).item())
@@ -253,8 +254,11 @@ def train():
             with torch.no_grad():
                 fake = netG(fixed_noise).detach().cpu()
             img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+            #print img list 
+            print('fake img list\n',img_list)
 
         iters += 1
+        break
     
     plt.figure(figsize=(10,5))
     plt.title("Generator and Discriminator Loss During Training")
@@ -275,6 +279,8 @@ def train():
     plt.title("Real Images")
     plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
     plt.savefig("dcgan/realimg.png")
+    #print plot real img
+    print('np.transpose' ,np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
 
     # Plot the fake images from the last epoch
     plt.subplot(1,2,2)
