db7046484d3a763051a99bd4c5a606f5a2538a28
zhouzp2115 <1169605579@qq.com>
2019-04-11 13:56:58 +0800

    para count

--- cifar-10/GAN-Train.py
+++ cifar-10/GAN-Train.py
@@ -39,7 +39,7 @@ def train():
     Gan = CIFAR10_Net(2)
     for index, data in enumerate(trainloader):
         print('batch_', index)
-        Gan.adjust_learning_rate(index)
+        Gan.adjust_learning_rate(index+1)
         Gan.train(data[0])
     print('train over')
     Gan.plotloss('loss.png')
