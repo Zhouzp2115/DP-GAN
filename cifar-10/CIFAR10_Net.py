5b4e3986b9a899a99599ea77c9bd2e57ea92aaf9
zhouzp2115 <1169605579@qq.com>
2019-04-12 09:59:54 +0800

    resize

--- cifar-10/CIFAR10_Net.py
+++ cifar-10/CIFAR10_Net.py
@@ -215,6 +215,38 @@ class CIFAR10_Net():
         D_losses_batch = []
         noise = []
 
+        # test grad
+        batch_data.to(self.device)
+        print(batch_data.size())
+        item = batch_data[0]
+        print(item.size())
+        label = torch.full((50,), real_label, device=self.device)
+        label_item = label[0]
+
+        self.netD.zero_grad()
+        output = self.netD(batch_data).view(-1)
+        errD_real = self.criterion(output, label)
+        errD_real.backward()
+
+        for parameter in self.netD.parameters():
+            print(parameter.grad)
+            print(parameter.grad.size())
+            break
+
+        self.netD.zero_grad()
+        item = item.reshape(1, 3, 64, 64)
+        label = torch.full((1,), real_label, device=self.device)
+        output = self.netD(item).view(-1)
+        errD_real = self.criterion(output, label)
+        errD_real.backward()
+
+        for parameter in self.netD.parameters():
+            print(parameter.grad)
+            print(parameter.grad.size())
+            break
+
+        exit()
+
         for index, data in enumerate(batch_data):
             D_grad_item = []
             data = data.reshape(1, 3, 64, 64)
--- cifar-10/GAN-Train.py
+++ cifar-10/GAN-Train.py
@@ -38,13 +38,14 @@ def train(model_num, start_num, end_num):
     ])
 
     CIFARDataset = CIFARDataLoader('../data/cifar-10/sorted/train_' + str(model_num), transform)
-    trainloader = torch.utils.data.DataLoader(CIFARDataset, batch_size=1, shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(CIFARDataset, batch_size=50, shuffle=True, num_workers=2)
 
     Gan = CIFAR10_Net(model_num, start_num, end_num)
 
     G_loss = []
     D_loss = []
     epoch_num = 2
+
     for epoch in range(epoch_num):
         for index, data in enumerate(trainloader):
             # print('[%d/%d] batch_%d'%(epoch ,epoch_num ,index))
